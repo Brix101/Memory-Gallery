a1d5f573548c587f17f4aaff52b460b7eb235db4
Brixter Porras <brixterporras@gmail.com>
2022-07-07 20:31:07 +0800

    Added Admin Layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./components/adminComponents/Admin";
 import NavigationBar from "./components/NavBar/NavigationBar";
 import About from "./pages/About";
-import Admin from "./pages/Admin";
 import Auth from "./pages/auth/Auth";
 import Contact from "./pages/Contact";
+import Dashboard from "./pages/Dashboard";
 import Gallery from "./pages/Gallery";
 import Home from "./pages/Home";
+import Page404 from "./pages/Page404";
 
 function App() {
   return (
@@ -19,7 +21,10 @@ function App() {
           <Route path="contact" element={<Contact />} />
         </Route>
         <Route path="login" element={<Auth />} />
-        <Route path="admin" element={<Admin />} />
+        <Route path="admin" element={<Admin />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   );
