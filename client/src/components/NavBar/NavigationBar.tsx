e33f0dcae3a9a61ee2912e6dd24acf331379f350
Brixter Porras <brixterporras@gmail.com>
2022-07-07 17:35:52 +0800

    refactor navigationbar components

---
+++ client/src/components/NavBar/NavigationBar.tsx
@@ -0,0 +1,123 @@
+/* This example requires Tailwind CSS v2.0+ */
+import React, { Fragment } from "react";
+import { Outlet, Link, useLocation } from "react-router-dom";
+
+import { Disclosure, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+import galleryImage from "../../assets/gallery.png";
+
+interface navItem {
+  name: string;
+  href: string;
+  current?: boolean;
+}
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function NavigationBar() {
+  const { state } = useLocation();
+
+  const navigation: navItem[] = [
+    { name: "Home", href: "/", current: !state ? true : state === "/" },
+    { name: "Gallery", href: "gallery", current: state === "gallery" },
+    { name: "About Us", href: "about", current: state === "about" },
+    { name: "Contact", href: "contact", current: state === "contact" },
+    { name: "Login", href: "login" },
+  ];
+
+  return (
+    <>
+      <Disclosure as="nav" className="bg-gray-800 shadow">
+        {({ open }) => (
+          <>
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+              <div className="relative flex items-center justify-between h-16">
+                <div className="flex-1 flex items-stretch justify-between">
+                  {/* Logo */}
+                  <div className="flex-shrink-0 flex items-center">
+                    <Link to="/" state="/">
+                      <img
+                        className="block h-10 hover:bg-gray-700 px-1 rounded-md"
+                        src={galleryImage}
+                        alt="Gallery"
+                      />
+                    </Link>
+                  </div>
+                  {/* Nav Item */}
+                  <div className="hidden sm:block sm:ml-6">
+                    <div className="flex space-x-4">
+                      {navigation.map((item) => (
+                        <Link
+                          key={item.name}
+                          to={item.href}
+                          state={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-900 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "px-3 py-2 rounded-md text-sm font-medium"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+              </div>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Disclosure.Panel className="sm:hidden">
+                <div className="px-2 pt-2 pb-3 space-y-1">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      to={item.href}
+                      state={item.href}
+                      className={classNames(
+                        item.current
+                          ? "bg-gray-900 text-white"
+                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                        "block px-3 py-2 rounded-md text-base font-medium"
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+              </Disclosure.Panel>
+            </Transition>
+          </>
+        )}
+      </Disclosure>
+
+      {/* Page Views */}
+      <Outlet />
+    </>
+  );
+}
