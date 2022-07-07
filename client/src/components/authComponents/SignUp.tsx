bcda9c7a928b8879bf048db1ec7cb337eb25f0f2
Brixter Porras <brixterporras@gmail.com>
2022-07-07 17:42:10 +0800

    Change the sign up text

--- client/src/components/authComponents/SignUp.tsx
+++ client/src/components/authComponents/SignUp.tsx
@@ -19,7 +19,7 @@ function SignUp({ signInMode }: any) {
               />
             </Link>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Sign Up your new Account
+              Sign Up a new Account
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               Or a Member?{" "}
