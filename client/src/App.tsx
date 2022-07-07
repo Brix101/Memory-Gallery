0349637c0692e61081cc090225a6aee69413ad22
Brixter Porras <brixterporras@gmail.com>
2022-07-07 14:39:06 +0800

    Added Login Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar";
+import NavigationBar from "./components/NavBar/NavigationBar";
 import About from "./pages/About";
+import Admin from "./pages/Admin";
+import Auth from "./pages/auth/Auth";
 import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
 import Home from "./pages/Home";
@@ -16,6 +18,8 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Route>
+        <Route path="login" element={<Auth />} />
+        <Route path="admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
