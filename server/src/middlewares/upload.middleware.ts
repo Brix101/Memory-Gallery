a6fa1eb173e1d3b7226d08af4ab3b148988a0e58
Brixter Porras <brixterporras@gmail.com>
2022-07-13 01:42:44 +0800

    Refactor Cloudinary config

--- server/src/middlewares/upload.middleware.ts
+++ server/src/middlewares/upload.middleware.ts
@@ -1,14 +1,7 @@
 import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import { v2 as cloudinary } from 'cloudinary';
-import { CLOUD_API_KEY, CLOUD_API_SECRET, CLOUD_FOLDER, CLOUD_NAME } from '@/config';
-
-cloudinary.config({
-  cloud_name: CLOUD_NAME,
-  api_key: CLOUD_API_KEY,
-  api_secret: CLOUD_API_SECRET,
-  secure: true,
-});
+import cloudinary from '@/utils/cloudinary';
+import { CLOUD_FOLDER } from '@/config';
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
@@ -31,6 +24,6 @@ const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
   limits: { fileSize: maxSize },
-}).array('image', 10);
+});
 
 export default upload;
