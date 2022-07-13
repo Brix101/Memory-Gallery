612ee76e4c49832f3f18003693d4b7814d95e657
Brixter Porras <brixterporras@gmail.com>
2022-07-13 23:56:48 +0800

    Update Image responsive

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -38,34 +38,37 @@ function Home() {
             width: "100%",
             height: "auto",
             gridTemplateColumns:
-              "repeat(auto-fill, minmax(250px, 1fr))!important",
+              "repeat(auto-fill, minmax(300px, 1fr))!important",
           }}
           gap={20}
         >
           {images?.map((image: Image, i) => (
-            <ImageListItem key={i}>
+            <ImageListItem key={i} className="group rounded-md cursor-pointer">
               <img
+                className="rounded-md"
                 src={image.path}
                 alt={image.originalname}
                 loading="lazy"
                 style={{
                   width: "auto",
-                  height: " 300px",
+                  height: "20rem",
                   objectFit: "fill",
                 }}
               />
-              <ImageListItemBar
-                title={image.title}
-                // subtitle={item.author}
-                actionIcon={
-                  <IconButton
-                    sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                    aria-label={`info about ${image.title}`}
-                  >
-                    <InfoIcon />
-                  </IconButton>
-                }
-              />
+              <div className="hidden group-hover:block duration-700">
+                <ImageListItemBar
+                  title={image.title}
+                  // subtitle={item.author}
+                  actionIcon={
+                    <IconButton
+                      sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                      aria-label={`info about ${image.title}`}
+                    >
+                      <InfoIcon />
+                    </IconButton>
+                  }
+                />
+              </div>
             </ImageListItem>
           ))}
         </ImageList>
