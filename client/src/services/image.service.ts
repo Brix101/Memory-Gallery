bc52e8a53778e3feaed88ad453fd252013611c62
Brixter Porras <brixterporras@gmail.com>
2022-07-13 23:18:46 +0800

    Added Image Query

--- client/src/services/image.service.ts
+++ client/src/services/image.service.ts
@@ -1,8 +1,9 @@
 import { api } from "../app/api";
+import { Image } from "../interfaces";
 
 const imageEndPoints = api.injectEndpoints({
   endpoints: (build) => ({
-    getImages: build.query({
+    getImages: build.query<Image[], string>({
       query: () => ({
         url: "/images",
       }),
