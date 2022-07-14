45bdd5ad2c56e69e23fbb4f85889200e8aa55008
Brixter Porras <brixterporras@gmail.com>
2022-07-15 00:21:15 +0800

    Added Account Design

--- client/src/pages/admin/Account.tsx
+++ client/src/pages/admin/Account.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import AccountProfile from "../../components/adminComponents/AccountProfile";
+import AccountProfileDetails from "../../components/adminComponents/AccountProfileDetails";
 
 function Account() {
-  return <div>Account</div>;
+  return (
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+      }}
+    >
+      <Container maxWidth={false}>
+        <Typography sx={{ mb: 3 }} variant="h4">
+          Account
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item lg={4} md={6} xs={12}>
+            <AccountProfile />
+          </Grid>
+          <Grid item lg={8} md={6} xs={12}>
+            <AccountProfileDetails />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
 
 export default Account;
